af4babb1db13cd8718f3ca3ed83a0b1fc3bf0bba
kmiller <kmmiller418@gmail.com>
2022-05-05 14:21:09 -0400

    Add StopAtFive

---
+++ stop-at-five/src/StopAtFive.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StopAtFive {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+
+        int number = 0;
+
+        while (number != 5){
+            System.out.println("Give a numbr:");
+            number = s.nextInt();
+        }
+    }
+}
