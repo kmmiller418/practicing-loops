7dfd5aa9abf85d035411af53546423572a808dbc
kmiller <kmmiller418@gmail.com>
2022-05-05 16:16:02 -0400

    Add conditional if user enters number > 11

--- count-to-eleven/src/CountToEleven.java
+++ count-to-eleven/src/CountToEleven.java
@@ -4,8 +4,11 @@ public class CountToEleven {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter a number less than 11");
-        int i = s.nextInt();
+        int i = 11;
+        while (i >= 11){
+            System.out.println("Enter a number less than 11:");
+            i = s.nextInt();
+        }
 
         for (; i <= 11; i++){
             System.out.println(i);
